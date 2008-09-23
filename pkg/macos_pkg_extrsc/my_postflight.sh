4fd31920a5a9ab81f2cb41b1048c84e3972a0bc5
Jerome Etienne <jerome.etienne@gmail.com>
2008-09-23 18:00:51 +0200

    - macos: some fix on the path of iceberg postflight path 	modified:   macos_pkg_extrsc/my_postflight.sh

--- pkg/macos_pkg_extrsc/my_postflight.sh
+++ pkg/macos_pkg_extrsc/my_postflight.sh
@@ -6,10 +6,11 @@
 
 # make the symlink to get it started at boot time
 # - only for SYSBOOT apps
-ln -sf /Applications/neoip-webpack/org.neoip.webpack.plist /Library/LaunchDaemons
+ln -sf /Applications/neoip-webpack/pkg_extrsc/org.neoip.webpack.plist /Library/LaunchDaemons
 
 # TODO what if it is already installed ?
-# - i should stop the previous one
+# - i should unload the previous one if it is loaded
+# - i should stop the previous one if it is running
 
 # Start the apps
 launchctl load /Library/LaunchDaemons/org.neoip.webpack.plist
