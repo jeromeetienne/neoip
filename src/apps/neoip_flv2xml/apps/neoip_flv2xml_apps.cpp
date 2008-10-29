e7e127d2c392fc99e84b130c8876d531c78f467f
Jerome Etienne <jerome.etienne@gmail.com>
2008-10-29 22:48:48 +0100

    about rtmp_resp_t stuff: - in the adaptation, i renamed a amf class, so this file has been modified

--- src/apps/neoip_flv2xml/apps/neoip_flv2xml_apps.cpp
+++ src/apps/neoip_flv2xml/apps/neoip_flv2xml_apps.cpp
@@ -5,10 +5,10 @@
 flv2xml_apps_t read .flv data from stdin and output the flv format into xml.
 It is handcrafted xml but it is supposed to be valid one. aka parsable by
 xml parser.
-- it output all the info obtained by flv_parse_t 
+- it output all the info obtained by flv_parse_t
 - additionnaly it output a special element <keyframe> for each keyframe
 
-\par TODO - the xml format is unclear. 
+\par TODO - the xml format is unclear.
   - the goal is to display all available info BUT to do it in a easyly parsable format
   - the current one fit current need and is generic... so not that bad for now
 
@@ -51,7 +51,7 @@ flv2xml_apps_t::flv2xml_apps_t()	throw()
 	m_fdwatch	= NULL;
 	m_flv_parse	= NULL;
 }
-	
+
 /** \brief Destructor
  */
 flv2xml_apps_t::~flv2xml_apps_t()	throw()
@@ -80,7 +80,7 @@ flv_err_t	flv2xml_apps_t::start()	throw()
 	const strvar_db_t &	arg_option	= lib_apps->arg_option();
 	// log to debug
 	KLOG_INFO("enter");
-	
+
 	// set all output options to false
 	output_tophd	= output_tag_audio	= false;
 	output_tag_video= output_tag_meta	= false;
@@ -92,13 +92,13 @@ flv_err_t	flv2xml_apps_t::start()	throw()
 	if( arg_option.contain_key("tag_meta") )	output_tag_meta		= true;
 	if( arg_option.contain_key("custom_kframe") )	output_custom_kframe	= true;
 	// if no option has been set, set them all
-	if( !output_tophd && !output_tag_audio && !output_tag_video 
+	if( !output_tophd && !output_tag_audio && !output_tag_video
 					&& !output_tag_meta && !output_custom_kframe){
 		output_tophd	= output_tag_audio	= true;
 		output_tag_video= output_tag_meta	= true;
 		output_custom_kframe			= true;
 	}
-	
+
 	// start the fdwatch on stdin
 	m_fdwatch	= nipmem_new fdwatch_t();
 	m_fdwatch->start(0, fdwatch_t::INPUT | fdwatch_t::ERROR, this, NULL);
@@ -108,7 +108,7 @@ flv_err_t	flv2xml_apps_t::start()	throw()
 	m_flv_parse	= nipmem_new flv_parse_t();
 	flv_err		= m_flv_parse->start(this, NULL);
 	if( flv_err.failed() )	return flv_err;
-	
+
 	// output the xml result - here only a standard xml header
 	KLOG_STDOUT("<?xml version=\"1.0\" encode=\"UTF-8\"?>\n");
 	KLOG_STDOUT("<flv>\n");
@@ -122,7 +122,7 @@ flv_err_t	flv2xml_apps_t::start()	throw()
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 /** \brief Exit the apps as soon as possible
- * 
+ *
  * @return true in all case (only for tokeep convenience)
  */
 bool	flv2xml_apps_t::exit_asap(const flv_err_t &flv_err = flv_err_t::OK)	throw()
@@ -139,7 +139,7 @@ bool	flv2xml_apps_t::exit_asap(const flv_err_t &flv_err = flv_err_t::OK)	throw()
 	lib_session_get()->loop_stop_asap();
 
 	// stop the post_fdwatch_zerotimer, if it exist
-	post_fdwatch_zerotimer.remove(this, NULL);	
+	post_fdwatch_zerotimer.remove(this, NULL);
 	// delete the fdwatch if needed
 	nipmem_zdelete m_fdwatch;
 	// delete flv_parse_t if needed
@@ -156,16 +156,16 @@ bool	flv2xml_apps_t::exit_asap(const std::string &reason)	throw()
 }
 
 /** \brief Return true if the parsing completed, false otherwise
- * 
+ *
  * - the parsing is considered completed when:
  *   1. stdin is closed (aka m_fdwatch == NULL)
  *   2. flv_parse_t buffer is empty
- */ 
+ */
 bool	flv2xml_apps_t::parsing_completed(const flv_parse_event_t &parse_event)	throw()
 {
 	// compute the amount of unparsed data in flv_parse->buffer()
 	size_t	unparsed_len	= m_flv_parse->buffer().size();
-	
+
 	// if a flv_parse_event_t is specified, take its length into account
 	if( !parse_event.is_null() ){
 		DBG_ASSERT( m_flv_parse->buffer().size() >= parse_event.byte_length() );
@@ -176,11 +176,11 @@ bool	flv2xml_apps_t::parsing_completed(const flv_parse_event_t &parse_event)	thr
 	KLOG_DBG("unparse_len=" << unparsed_len);
 
 	// if unparsed_len == 0 and no more data to read, leave asap
-	// - do a -4 because it should point to the flv_taghd_t::type and 
+	// - do a -4 because it should point to the flv_taghd_t::type and
 	//   not the flv_taghd_t::prevtag_size which is an uint32_t
-	//   - this flv_taghd_t::prevtag_size crap is as well in 
+	//   - this flv_taghd_t::prevtag_size crap is as well in
 	//     flv_parse_helper_t::kframe_boffset_from(), in the flv_parse_t nunit
-	//     and neoip-flv2xml 
+	//     and neoip-flv2xml
 	//   - i dont like it, seems like a bug in the flv_parse_t architechture
 	//   - it is spreaded everywhere, likely a misunderstanding of the flv format
 	if( unparsed_len <= sizeof(uint32_t) && !m_fdwatch )	return true;
@@ -206,7 +206,7 @@ bool	flv2xml_apps_t::neoip_fdwatch_cb(void *cb_userptr, const fdwatch_t &cb_fdwa
 
 	// sanity check - the fdwatch_t::INPUT condition MUST the one notified
 	DBG_ASSERT( cond.is_input() );
-	
+
 	// allocate the buffer to store read data - in stack for better speed
 	size_t	buf_len	= 128*1024;
 	void *	buf_ptr	= nipmem_alloca(buf_len);
@@ -241,7 +241,7 @@ bool	flv2xml_apps_t::neoip_fdwatch_cb(void *cb_userptr, const fdwatch_t &cb_fdwa
 		post_fdwatch_zerotimer.append(this, NULL);
 		// if not yet completed, let flv_parse_t complete
 		return	false;
-	}	
+	}
 
 	// return tokeep
 	return true;
@@ -263,14 +263,14 @@ bool	flv2xml_apps_t::neoip_zerotimer_expire_cb(zerotimer_t &cb_zerotimer, void *
 	// sanity check - the m_fdwatch MUST be deleted at this point
 	DBG_ASSERT( m_fdwatch == NULL );
 	// sanity check - at this point, m_flv_parse MUST NOT be completed
-	DBG_ASSERT( !parsing_completed(flv_parse_event_t()) ); 
+	DBG_ASSERT( !parsing_completed(flv_parse_event_t()) );
 
 	// NOTE: this point is reached IIF m_fdwatch is closed and flv_parse_t is still
 	//       expecting data. so it is a bug in the input
-	
+
 	// exit with an error
 	exit_asap("broken end of the input. input closed and flv_parse_t still waiting for data");
-	
+
 	// return tokeep
 	return true;
 }
@@ -289,20 +289,20 @@ bool	flv2xml_apps_t::neoip_flv_parse_cb(void *cb_userptr, flv_parse_t &cb_flv_pa
 	flv_err_t	flv_err;
 	// log to debug
 	KLOG_DBG("enter event=" << parse_event);
-	
+
 	// if flv_parse_event_t is fatal, report an error
 	if( parse_event.is_fatal() )	return exit_asap(parse_event.get_error());
-	
+
 	// parse this event according to the flv_taghd.type()
 	switch( parse_event.value() ){
 	case flv_parse_event_t::TOPHD:	flv_err	= parse_event_tophd(parse_event);	break;
 	case flv_parse_event_t::TAG:	flv_err	= parse_event_tag(parse_event);		break;
 	default:	DBG_ASSERT(0);
 	}
-	
+
 	// display kframe if available
 	if( flv_err.succeed() )	flv_err	= display_kframe_ifavail(parse_event);
-	
+
 	// handle the error case
 	if( flv_err.failed() ){
 		std::string reason = "unabled to parse flv_parse_event_t " + parse_event.to_string() + " due to " + flv_err.to_string();
@@ -333,7 +333,7 @@ flv_err_t	flv2xml_apps_t::parse_event_tophd(const flv_parse_event_t &parse_event
 	flv_tophd_t	flv_tophd	= parse_event.get_tophd();
 	// sanity check - the parse_event MUST be a flv_parse_event_t::TOPHD
 	DBG_ASSERT( parse_event.is_tophd() );
-	
+
 	// log to debug
 	KLOG_INFO("flv_tophd=" << flv_tophd);
 
@@ -342,8 +342,8 @@ flv_err_t	flv2xml_apps_t::parse_event_tophd(const flv_parse_event_t &parse_event
 	// output the xml result
 	KLOG_STDOUT("<tophd>\n");
 	KLOG_STDOUT("<version>"		<< int(flv_tophd.version())		<< "</version>\n");
-	KLOG_STDOUT("<has_video>"	<< (flv_tophd.flag().is_video()? 1:0)	<< "</has_video>\n");		
-	KLOG_STDOUT("<has_audio>"	<< (flv_tophd.flag().is_audio()? 1:0)	<< "</has_audio>\n");		
+	KLOG_STDOUT("<has_video>"	<< (flv_tophd.flag().is_video()? 1:0)	<< "</has_video>\n");
+	KLOG_STDOUT("<has_audio>"	<< (flv_tophd.flag().is_audio()? 1:0)	<< "</has_audio>\n");
 	KLOG_STDOUT("</tophd>\n");
 
 	// return no error
@@ -360,8 +360,8 @@ flv_err_t	flv2xml_apps_t::parse_event_tag(const flv_parse_event_t &parse_event)	
 	// sanity check - the parse_event MUST be a flv_parse_event_t::TAG
 	DBG_ASSERT( parse_event.is_tag() );
 
-	
-	// parse this event according to the flv_taghd.type() 
+
+	// parse this event according to the flv_taghd.type()
 	switch( flv_taghd.type().get_value() ){
 	case flv_tagtype_t::AUDIO:	flv_err	= parse_tagtype_audio(flv_taghd, tag_data);
 					break;
@@ -374,7 +374,7 @@ flv_err_t	flv2xml_apps_t::parse_event_tag(const flv_parse_event_t &parse_event)	
 	}
 	// if an error occured, return now
 	if( flv_err.failed() )	return flv_err;
-	
+
 	// return no error
 	return flv_err_t::OK;
 }
@@ -393,7 +393,7 @@ flv_err_t	flv2xml_apps_t::parse_tagtype_audio(const flv_taghd_t &flv_taghd
 	flv_taghd_audio_t	taghd_audio;
 	// sanity check - flv_taghd_t MUST be a flv_tagtype_t::AUDIO
 	DBG_ASSERT( flv_taghd.type() == flv_tagtype_t::AUDIO );
-	
+
 	// parse the flv_taghd_audio_t from the tag_data
 	try {
 		bytearray_t	bytearray(tag_data);
@@ -405,7 +405,7 @@ flv_err_t	flv2xml_apps_t::parse_tagtype_audio(const flv_taghd_t &flv_taghd
 
 	// log to debug
 	KLOG_DBG("taghd_audio=" << taghd_audio);
-	
+
 	// if flv_tagtype_t::AUDIO MUST NOT be displayed, return now
 	if( !output_tag_audio )	return flv_err_t::OK;
 	// output the xml result
@@ -415,6 +415,11 @@ flv_err_t	flv2xml_apps_t::parse_tagtype_audio(const flv_taghd_t &flv_taghd
 	KLOG_STDOUT("<soundsize>"	<< taghd_audio.soundsize()	<< "</soundsize>\n");
 	KLOG_STDOUT("<soundrate>"	<< taghd_audio.soundrate()	<< "</soundrate>\n");
 	KLOG_STDOUT("<soundformat>"	<< taghd_audio.soundformat()	<< "</soundformat>\n");
+#if 0	// modification to output the data too
+	KLOG_STDOUT("<data>\n");
+	KLOG_STDOUT(tag_data << "\n");
+	KLOG_STDOUT("</data>\n");
+#endif
 	KLOG_STDOUT("</taghd_audio>\n");
 	display_taghd_end(flv_taghd);
 
@@ -430,7 +435,7 @@ flv_err_t	flv2xml_apps_t::parse_tagtype_video(const flv_taghd_t &flv_taghd
 	flv_taghd_video_t	taghd_video;
 	// sanity check - flv_taghd_t MUST be a flv_tagtype_t::VIDEO
 	DBG_ASSERT( flv_taghd.type() == flv_tagtype_t::VIDEO );
-	
+
 	// parse the flv_taghd_video_t from the tag_data
 	try {
 		bytearray_t	bytearray(tag_data);
@@ -452,6 +457,11 @@ flv_err_t	flv2xml_apps_t::parse_tagtype_video(const flv_taghd_t &flv_taghd
 	KLOG_STDOUT("<frametype>"	<< taghd_video.frametype()	<< "</frametype>\n");
 	KLOG_STDOUT("</taghd_video>\n");
 	display_taghd_end(flv_taghd);
+#if 0	// modification to output the data too
+	KLOG_STDOUT("<data>\n");
+	KLOG_STDOUT(tag_data << "\n");
+	KLOG_STDOUT("</data>\n");
+#endif
 	// return no error
 	return flv_err_t::OK;
 }
@@ -468,16 +478,16 @@ flv_err_t	flv2xml_apps_t::parse_tagtype_meta(const flv_taghd_t &flv_taghd
 	// sanity check - flv_taghd_t MUST be a flv_tagtype_t::META
 	DBG_ASSERT( flv_taghd.type() == flv_tagtype_t::META);
 	// get the event_type
-	flv_err	= flv_amf_parse_t::amf_to_dvar(bytearray, type_dvar);
+	flv_err	= amf0_parse_t::amf_to_dvar(bytearray, type_dvar);
 	if( flv_err.failed() )	return flv_err;
 
 	// get the event_data associated with the event_type
-	flv_err	= flv_amf_parse_t::amf_to_dvar(bytearray, data_dvar);
+	flv_err	= amf0_parse_t::amf_to_dvar(bytearray, data_dvar);
 	if( flv_err.failed() )	return flv_err;
 
 	// log to debug
 	KLOG_INFO("METADATA event_type=" << type_dvar << " event_data=" << data_dvar);
-	
+
 	// if flv_tagtype_t::META MUST NOT be displayed, return now
 	if( !output_tag_meta )	return flv_err_t::OK;
 	// display the event
