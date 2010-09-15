2ab718075aad6d6493835e47729b791c11b2188f
Jerome Etienne <jerome.etienne@gmail.com>
2010-09-15 10:10:20 +0200

    neoip_dnsgrab/libnss: streamlining libnss installation to port in ppa_install 	modified:   src/neoip_dnsgrab/libnss/Makefile 	new file:   src/neoip_dnsgrab/libnss/script_postinst.sh 	new file:   src/neoip_dnsgrab/libnss/script_prerm.sh

---
+++ src/neoip_dnsgrab/libnss/script_postinst.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+doinstall_nsswitch() {
+	# setup some variable
+	local ORIGIN_FNAME="/etc/nsswitch.conf"
+	local BACKUP_FNAME="/etc/nsswitch.conf.pre_install.libnss_neoip.backup"
+
+	# test if it is nss is already in nssswitch.conf
+	grep -e "^hosts:.*neoip.*" $$ORIGIN_FNAME >/dev/null
+	local not_present="$$?"	
+	# if already present, return now
+	if [ "$$not_present" = "0" ]; then
+		return 0
+	fi
+		
+	# copy the original file into the backup one
+	echo "backup $$ORIGIN_FNAME into $$BACKUP_FNAME"
+	cp $$ORIGIN_FNAME $$BACKUP_FNAME
+	# modify ORIGIN_FNAME to add the 'neoip' in hosts:
+	echo "modify $$ORIGIN_FNAME to add 'neoip' to the hosts: line"
+	sed 's/\(^hosts:.*files\)\(.*\)/\1 neoip\2/' < $$BACKUP_FNAME > $$ORIGIN_FNAME
+}
+
+# update the nss_switch
+doinstall_nsswitch
+
---
+++ src/neoip_dnsgrab/libnss/script_prerm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+uninstall_nsswitch() {
+	# setup some variable
+	local ORIGIN_FNAME="/etc/nsswitch.conf"
+	local BACKUP_FNAME="/etc/nsswitch.conf.pre_uninstall.libnss_neoip.backup"
+
+	# test if it is nss is already in nssswitch.conf
+	grep -e "^hosts:.*neoip.*" $$ORIGIN_FNAME >/dev/null
+	local not_present="$$?"
+	# if not present, no need to uninstall so return now
+	if [ "$$not_present" = "1" ]; then
+		return 0
+	fi
+	
+	# copy the original file into the backup one
+	echo "backup $$ORIGIN_FNAME into $$BACKUP_FNAME"
+	cp $$ORIGIN_FNAME $$BACKUP_FNAME
+	# modify ORIGIN_FNAME to remove the 'neoip' in hosts:
+	echo "modify $$ORIGIN_FNAME to remove 'neoip' from the hosts: line"
+	sed 's/\(^hosts:.*\)neoip[ ]\(.*\)/\1\2/' < $$BACKUP_FNAME > $$ORIGIN_FNAME
+}
+
+# update the nss_switch
+uninstall_nsswitch
