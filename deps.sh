baf6e02f76cd4aaf8c2bbf65dec1b92fbc1b81a7
jerome <jerome@jmebox.(none)>
2010-12-08 20:03:49 +0100

    added a /deps.sh to record all the dependancies 	new file:   deps.sh

---
+++ deps.sh
@@ -0,0 +1,7 @@
+# list of dependancies to compile neoip
+sudo apt-get install g++
+sudo apt-get install libglib2.0-dev
+sudo apt-get install libgcrypt11-dev
+sudo apt-get install libgnutls-dev
+sudo apt-get install libidn11-dev
+
